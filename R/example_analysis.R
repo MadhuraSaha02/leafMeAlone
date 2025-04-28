1c78485f7e505ac82b151dd2e1e2ce53e1978448
Madhura Saha <madhura.saha02@gmail.com>
2025-04-28 03:16:18 +0200

    fix: update run_example_analysis to load NDVI from extdata

--- R/example_analysis.R
+++ R/example_analysis.R
@@ -4,10 +4,13 @@
 #'
 #' @export
 run_example_analysis <- function() {
-  data <- download_satellite_data()
-  onset_map <- phenology_map(data$ndvi_stack, data$dates)
-  plot(onset_map, main = "Autumn Onset - Vancouver")
-  pixel_ndvi <- as.numeric(terra::extract(data$ndvi_stack, matrix(c(500, 500), ncol = 2)))
-  phen <- extract_phenology_metrics(pixel_ndvi, data$dates)
+  ndvi_stack <- terra::rast(system.file("extdata", "ndvi_stack.tif", package = "leafMeAlone"))
+  dates <- as.Date(scan(system.file("extdata", "ndvi_dates.txt", package = "leafMeAlone"), what = "character"))
+
+  onset_map <- phenology_map(ndvi_stack, dates)
+  plot(onset_map, main = "Estimated Autumn Onset (DOY)")
+
+  pixel_ndvi <- as.numeric(terra::extract(ndvi_stack, matrix(c(500, 500), ncol = 2)))
+  phen <- extract_phenology_metrics(pixel_ndvi, dates)
   plot_phenology_curve(phen$ndvi_curve, phen$autumn_onset_doy)
 }
